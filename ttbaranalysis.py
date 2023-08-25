6c58e860b759d7be42160cc14d5cb32c328c40e9
acwillia <acwillia@buffalo.edu>
2023-08-25 18:51:14 -0500

    Preped the analysis for making deepAK8 coffea outputs

--- ttbaranalysis.py
+++ ttbaranalysis.py
@@ -214,9 +214,10 @@ if __name__ == "__main__":
 
                 # coffea output file name
                 subString = subsection.replace('700to', '_700to').replace('1000to','_1000to')
-                if args.bkgest: subString += '_bkgest_NoMassMod' # Temporarily turned off mod mass
+                if args.bkgest: subString += '_bkgest'
                 if args.test: subString += '_test'
                 # subString += '_blinded'  # Temporary labeling convention for my (AC) personal use
+                subString += '_DeepAK8' # Temporary labeling for making outputs with deepAK8 tagger
                                 
                 savefilename = f'{savedir}{sample}_{IOV}{subString}.coffea'
                 if 'RSGluon' in sample:
