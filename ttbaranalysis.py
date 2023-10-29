7aa764e5e15859339d769f27521d91b5bb010411
acwillia <acwillia@buffalo.edu>
2023-10-29 12:16:06 -0500

    Optional subString can be added at the end of coffea filename for clearer identification of which HT cut and btagger was used

--- ttbaranalysis.py
+++ ttbaranalysis.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     
     # choose specific eras, pt bins, mass points
     parser.add_argument('--era', choices=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], action='append', default=[], help='--era A --era B --era C for multiple eras, runs all eras if not specificed')
-    parser.add_argument('-p', '--pt', choices=['700to1000', '1000toInf', '15to7000', '1000to1400', '1400to1800', '1800to2400', '2400to3200'], action='append', default=[], help='pt bins for TTbar or QCD datasets')
+    parser.add_argument('-p', '--pt', choices=['700to1000', '1000toInf', '15to7000', '800to1000', '1000to1400', '1400to1800', '1800to2400', '2400to3200', '3200toInf'], action='append', default=[], help='pt bins for TTbar or QCD datasets')
     parser.add_argument('-m', '--mass', action='append', default=[], help='mass points for signal')
 
     # analysis options
@@ -125,7 +125,7 @@ if __name__ == "__main__":
         'pileup',
         'pdf',
         'q2',
-        # 'btag',
+        'btag',
         'toptagsf',
         'toptagxs',
         'lumi'
@@ -238,7 +238,7 @@ if __name__ == "__main__":
                 fileset = {sample: files}            
 
                 # coffea output file name
-                subString = subsection.replace('700to', '_700to').replace('1000to','_1000to').replace('15to','_15to').replace('1400to','_1400to').replace('1800to','_1800to').replace('2400to','_2400to')
+                subString = subsection.replace('700to', '_700to').replace('1000to','_1000to').replace('15to','_15to').replace('800to1000', '_800to1000').replace('1400to','_1400to').replace('1800to','_1800to').replace('2400to','_2400to').replace('3200to','_3200to')
 #                 if args.OW: subString += '_OW'
                 if args.noSyst: subString += '_noSyst'
                 if args.sideband: subString += '_sbTagDef'
@@ -248,7 +248,7 @@ if __name__ == "__main__":
                     subString += '_blinded'
                 if useDeepAK8: subString += '_DeepAK8' # Labeling for making outputs with deepAK8 tagger
                 if not MassModOn: subString += '_noMassMod'
-                subString += '_oldHTcut_oldBTag' # Temp label for testing with old HT cut of 950
+                # subString += '_oldHTcut' # Temp label for testing with old HT cut of 950
                                 
                 savefilename = f'{savedir}{sample}_{IOV}{subString}.coffea'
                 if 'RSGluon' in sample:
@@ -269,7 +269,7 @@ if __name__ == "__main__":
                         processor_instance=TTbarResProcessor(
                                                              iov=IOV,
                                                              bkgEst=args.bkgest,
-                					     Sideband=args.sideband,
+                                                             Sideband=args.sideband,
                                                              noSyst=args.noSyst,
                                                              blinding=Blinding,
                                                              MassMod=MassModOn,
@@ -369,3 +369,6 @@ if __name__ == "__main__":
     elapsed = time.time() - tic
     print(f"\nFinished in {elapsed:.1f}s")
     print(f"Events/s: {metrics['entries'] / elapsed:.0f}")
+    print('\n\n')
+    print(output['cutflow'])
+    print('\n\n')
